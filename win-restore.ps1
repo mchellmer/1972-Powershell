45a383d2bc6d9aa5a816bd99b40d2e95b5536114
Mark <mchellmer@gmail.com>
2023-04-12 12:40:24 +0100

    win restore script

---
+++ win-restore.ps1
@@ -0,0 +1,8 @@
+# appstore -> App Installer
+# set background: "C:\Users\mchel\OneDrive\Pictures\ff6wallpaper243440x1440.jpg"
+winget install -e --id Git.Git
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Google.Chrome
+winget install -e --id Microsoft.PowerShell
+winget install --id=OBSProject.OBSStudio  -e
+winget install -e --id GIMP.GIMP
