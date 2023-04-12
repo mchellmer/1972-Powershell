382db6d21dc1623cdc992fa6f55e9c091458b3d8
Mark <mchellmer@gmail.com>
2023-04-12 18:21:10 +0100

    7zip

--- win-restore.ps1
+++ win-restore.ps1
@@ -4,5 +4,11 @@ winget install -e --id Git.Git
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Google.Chrome
 winget install -e --id Microsoft.PowerShell
+winget install -e --id 7zip.7zip
+
+# Twitch
+## Install font: "C:\Users\mchel\OneDrive\Documents\0_Store\Twitch\04b_30\04B_30__.TTF"
+## Streamer.Bot https://streamer.bot/api/releases/streamer.bot/latest/download
 winget install --id=OBSProject.OBSStudio  -e
+## unzip "C:\Users\mchel\OneDrive\Documents\0_Store\Twitch\Obs\230411_obs-studio.zip" to C:\Users\mchel\AppData\Roaming\obs-studio
 winget install -e --id GIMP.GIMP
