3e8ebc5e2a8b72b724a847a794c1a1e6a2eb82d7
Mark <mchellmer@gmail.com>
2023-04-13 08:39:42 +0100

    archive

---
+++ twitch-backup.ps1
@@ -0,0 +1,22 @@
+# Get drives
+$archiveA = Get-Volume -FileSystemLabel "ArchiveA"
+$archiveB = Get-Volume -FileSystemLabel "ArchiveB"
+$archiveAPath = "$($archiveA.DriveLetter):"
+$archiveBPath = "$($archiveB.DriveLetter):"
+
+# Twitch Folder
+$twitchPath = "C:/Users/mchel/OneDrive/Documents/0_Store/Twitch/*"
+Copy-Item -Path $twitchPath -Destination "$archiveAPath/Twitch" -Recurse -Force
+Copy-Item -Path "$archiveAPath/Twitch" -Destination "$archiveBPath/Twitch" -Recurse -Force
+
+# Obs Folder
+$obsPath = "C:/Users/mchel/AppData/Roaming/obs-studio/*"
+Copy-Item -Path $obsPath -Destination "$archiveAPath/Obs" -Recurse -Force
+Copy-Item -Path "$archiveAPath/Obs" -Destination "$archiveBPath/Obs" -Recurse -Force
+
+# Recordings
+$recordingsPath = "C:/Users/mchel/Videos/*"
+$excludeSource = Get-ChildItem -recurse $recordingsPath
+$excludeA = Get-ChildItem -recurse "$archiveAPath/Recordings"
+Copy-Item -Path $recordingsPath -Destination "$archiveAPath/Recordings" -Recurse -Exclude $excludeSource
+Copy-Item -Path "$archiveAPath/Recordings/*" -Destination "$archiveBPath/Recordings" -Recurse -Exclude $excludeA
